f2217755a2aed3e0919a71e77af713489c681c13
Alexander Fleischli <alex.fleischli@gmail.com>
2015-03-05 19:18:44 +0100

    Added gallery effect
    Gallery now opens if you click an image an changes to that image

--- javascript/script.js
+++ javascript/script.js
@@ -1,3 +1,15 @@
 $(document).ready(function() {
-	
+	$('#imgblock img').click(function() {
+		$('.activeImg').not(this).removeClass('activeImg');
+
+		$('#gallery img').attr('src', $(this).attr('src'));
+
+		if ($(this).hasClass('activeImg') || $('#gallery').hasClass('hidden'))
+		{
+			$('#gallery').slideToggle();
+			$('#gallery').toggleClass('hidden');
+		}
+		
+		$(this).addClass('activeImg');
+	});
 });
